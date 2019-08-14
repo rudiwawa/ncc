423947a0a4dceb8581fa0cf4069b9af42d4335ae
rudiwawa <rudiwawa1980@gmail.com>
2019-08-14 16:52:43 +0700

    file js yang asli adalah yg di folder myjs_dev... folder myjs -> folder js versi build

---
+++ assets/myjs_dev/app.js
@@ -0,0 +1,203 @@
+$(document).ready(function () {
+
+	// $('#modal_crop').modal('hide');
+
+	// CONFIG URL AP
+	window.bashUrl = "/app";
+	window.url = new Array();
+	//PARIWISATA
+	window.url["pariwisata_jenis"] = window.bashUrl + "/index.php/Api_jenis_pariwisata";
+	window.url["pariwisata_sub_jenis"] = window.bashUrl + "/index.php/Api_sub_pariwisata";
+	window.url["pariwisata_konten"] = window.bashUrl + "/index.php/Api_konten_pariwisata";
+
+
+	function set_variable(i) {
+		window.url["ket_jenis_sub_get"] = window.bashUrl + "/index.php/Api_konten_" + i + "/ket_jenis_sub";
+		window.url["ket_jenis"] = window.bashUrl + "/index.php/Api_sub_" + i + "/ket_jenis";
+		window.url["data_byId_sub"] = window.bashUrl + "/index.php/Api_sub_" + i + "/data_byId";
+		window.url["data_byId_jenis"] = window.bashUrl + "/index.php/Api_jenis_" + i + "/data_byId";
+		window.url["data_byId_konten"] = window.bashUrl + "/index.php/Api_konten_" + i + "/data_byId";
+		window.url["get_sub_byId_jenis"] = window.bashUrl + "/index.php/Api_konten_" + i + "/get_sub_byId_jenis";
+	}
+	window.url["fasilitas_jenis"] = window.bashUrl + "/index.php/Api_jenis_fasilitas";
+	window.url["fasilitas_sub_jenis"] = window.bashUrl + "/index.php/Api_sub_fasilitas";
+	window.url["fasilitas_konten"] = window.bashUrl + "/index.php/Api_konten_fasilitas";
+
+	window.url["scroll"];
+	window.url["tmp_firstIdjenis"] = "dancok";
+
+
+
+	// ONCLICK EVENT!
+	$('#pariwisata_jenis').on('click', function (e) {
+		set_variable("pariwisata");
+		buildTableX("pariwisata_jenis");
+	});
+
+	$('#pariwisata_sub_jenis').on('click', function (e) {
+		set_variable("pariwisata");
+		buildTableX("pariwisata_sub_jenis");
+	});
+
+	$('#pariwisata_content').on('click', function (e) {
+		set_variable("pariwisata");
+		buildTableX("pariwisata_konten");
+	});
+
+	// FASILITAS
+	$('#fasilitas_jenis').on('click', function (e) {
+		set_variable("fasilitas");
+		buildTableX("fasilitas_jenis");
+	});
+
+	$('#fasilitas_sub_jenis').on('click', function (e) {
+		set_variable("fasilitas");
+		buildTableX("fasilitas_sub_jenis");
+	});
+
+	$('#fasilitas_content').on('click', function (e) {
+		set_variable("fasilitas");
+		buildTableX("fasilitas_konten");
+	});
+
+
+});
+
+var $loading = $('#loading_page').hide();
+$(document)
+	.ajaxStart(function () {
+		$loading.show();
+	})
+	.ajaxStop(function () {
+		$loading.hide();
+	});
+// var online = false;
+// // $('.user-profile').append('<span id="internet_status"></span>')
+// window.setInterval(function () {
+// 	var online = navigator.onLine;
+// 	// console.log(online);
+// 	if (online == false) {
+// 		// $('#internet_status').html('<p class="text-warning">Tidak Terhubung Internet</p>')
+// 		$(':button').prop('disabled', true); 
+// 	} else {
+// 		// $('#internet_status').html('<p class="text-success">Terhubung Internet</p>')
+// 		$(':button').prop('disabled', false); 
+// 	}
+// 	/// call your function here
+// }, 1000);
+
+//render table
+
+
+function buildTableX(tableX) {
+	$('#modal_crop').unbind();
+	$('#crop').off('click');
+
+
+	console.log(tableX + " clicked");
+	//load table
+	$("#content").load("./assets/contents/table/" + tableX + ".php", function () {
+		//load modelnya tableX
+		// $( "#divmodals" ).load( "./assets/contents/modal/"+tableX+"_update.php", function() {
+		$.getScript('./assets/myjs/table/' + tableX + '.js', function () {
+			buildTbody(tableX);
+		});
+		// });
+
+	});
+}
+
+function refreshTableX(tableX) {
+
+	//mencegah scroll saat
+	var clientHeight = $('#content').height();
+	$('#content').height(clientHeight);
+	return $("#content").load("./assets/contents/table/" + tableX + ".php", function () {
+		//load modelnya tableX
+
+		// $('#content').height(clientHeight);
+		buildTbody(tableX);
+
+
+	});
+}
+function refreshTableX(tableX, n) {
+	$("#content").load("./assets/contents/table/" + tableX + ".php", function () {
+		//load modelnya tableX
+
+		// $('#content').height(clientHeight);
+		buildTbody(tableX);
+	});
+}
+
+function ket_jenis_get() {
+	console.log("ket_jenis_get");
+	return $.ajax({
+		url: window.url["ket_jenis"],
+		type: "GET",
+		dataType: "JSON",
+		beforeSend: function () {
+			console.log("before send" + "url" + window.url["ket_jenis"]);
+		},
+		success: function (dataObject) {
+			if (dataObject.msg_main.status == true) {
+				$('#ket_jenis_select').empty();
+				console.log(dataObject.msg_detail.item);
+				var myjson = dataObject.msg_detail.item;
+				var is_insert = false;
+				$.each(myjson, function (key, value) {
+					$("select[name='id_jenis']").append("<option value=" + value.id_jenis + ">" + value.ket_jenis + "</option>");
+					console.log(value.id_jenis + "  " + value.ket_jenis);
+					if (is_insert == false) {
+						window.url["tmp_firstIdjenis"] = value.id_jenis;
+						is_insert = true;
+					}
+				});
+			}
+			else {
+				alert("register gagal \n" + dataObject.msg_detail.item);
+			}
+
+		},
+		complete: function () {
+			console.log("loading");
+
+		}
+	});
+}
+
+// function ket_sub_get(id) {
+// 	var json = { id };
+// 	var data;
+// 	console.log("get_placehorder" + id)
+// 	$.ajax({
+// 		url: window.url["get_sub_byId_jenis"],
+// 		type: "POST",
+// 		dataType: "JSON",
+// 		data: json,
+// 		beforeSend: function () {
+// 			console.log("before send get_placehorder");
+// 		},
+// 		success: function (dataObject) {
+// 			if (dataObject.msg_main.status == true) {
+// 				$('#id_sub').empty();
+// 				console.log(dataObject.msg_detail.item);
+// 				var myjson = dataObject.msg_detail.item;
+// 				$.each(myjson, function (key, value) {
+// 					$("select[name='id_sub']").append("<option value=" + value.id_sub + ">" + value.ket_sub_jenis + "</option>");
+// 					console.log(value.id_jenis + "  " + value.ket_jenis);
+// 				});
+// 			}
+// 			else {
+// 				alert("register gagal \n" + dataObject.msg_detail.item);
+// 			}
+
+// 		},
+// 		complete: function () {
+// 			console.log("loading");
+// 			// $("select[name='id_jenis']").hide().html(data).fadeIn('fast');
+
+// 		}
+// 	});
+
+// }
---
+++ assets/myjs_dev/table/pariwisata_sub_jenis.js
@@ -0,0 +1,274 @@
+var TableX;
+var ID;
+function buildTbody(tableX) {
+    TableX = tableX;
+    console.log("build TBODY " + tableX);
+    var number = 0;
+    $('#table_' + tableX).DataTable({
+        "processing": true,
+        // "ajax": window.url[tableX],
+        "ajax": { url: window.url[tableX], dataSrc: 'msg_detail.item' },//diperbaiki add dataSrc dan lokasi array nya yg ditandai [{}]
+        "columns": [
+            {
+                "render": function () {
+                    number++;
+                    return number;
+                }
+            },
+
+            { "data": "ket_jenis" },
+            { "data": "ket_sub_jenis" },
+            // { "msg_detail.item": "ket_sub_jenis" } ini salah ,
+            {
+                "render": function (data, type, JsonResultRow, meta) {
+                    return '<button class="btn btn-info edit_sub"  style="width: 40px; margin-right : 5px;" onclick ="update_modal(' + "'" + JsonResultRow.id_sub + "'" + ')"><i class="fa fa-pencil-square-o"></i></button>'
+                        + '<button class="btn btn-danger delete_sub" style="width: 40px;" onclick ="conf_delete(' + "'" + JsonResultRow.id_sub + "'" + ')"><i class="fa fa-trash-o"></i></button>'
+                        ;
+                }
+
+            }
+        ]
+    });
+}
+// //error solved
+
+$('#modal_sub').on('hidden.bs.modal', function (e) {
+    if (e.handled !== true) {
+        e.handled = true;
+        $('#modal_sub div').remove();
+        console.log("modal hidden");
+    }
+});
+
+function update_modal(id) {
+    ID = id;
+    $('.edit_sub').prop('disabled', true);
+    $("#modal_sub").load("./assets/contents/modal/" + TableX + "_update.php", function () {
+        $('#form')[0].reset(); // reset form on modals
+        $('.form-group').removeClass('has-error'); // clear error class
+        $('.help-block').empty(); // clear error string
+        console.log("insert_modal");
+        $('#modal_form_update').modal('show');
+        $.when(ket_jenis_get()).then(function (x) {
+            get_placehorder(id);
+        });
+        $('.edit_sub').prop('disabled', false);
+    });
+
+    // ID = id;
+    // save_method = 'update';
+    // $('#form')[0].reset(); // reset form on modals
+    // $('.form-group').removeClass('has-error'); // clear error class
+    // $('.help-block').empty(); // clear error string
+    // console.log("update_modal"+id);
+    // $.when(ket_jenis_get()).then(function( x ) {
+    //     get_placehorder(id);
+    // });
+}
+
+function insert_modal() {
+    $("#modal_sub").load("./assets/contents/modal/" + TableX + "_insert.php", function () {
+        $.when(ket_jenis_get()).then(function (x) {
+            $('#form')[0].reset(); // reset form on modals
+            $('.form-group').removeClass('has-error'); // clear error class
+            $('.help-block').empty(); // clear error string
+            console.log("insert_modal");
+            $('#modal_form_insert').modal('show');
+        });
+    });
+
+}
+
+function update_save() {
+    if ($("select[name='id_jenis']").length == 0) {
+        console.log("div gaada");
+    }
+    var id_sub = ID;
+    var id_jenis = $("select[name='id_jenis']").val();
+    var ket_sub_jenis = $("textarea[name='ket_sub_jenis']").val();
+    var json = { id_sub, id_jenis, ket_sub_jenis };
+    //Ajax Load data from ajax
+    console.log(ID);
+    console.log(json);
+    console.log(TableX);
+    $.ajax({
+        url: window.url[TableX],
+        type: "PUT",
+        dataType: "JSON",
+        headers: { "X-HTTP-Method-Override": "PUT" }, // X-HTTP-Method-Override set to PUT
+        data: json,
+        beforeSend: function () {
+            console.log("before send");
+            $("#content").append('')
+        },
+        success: function (dataObject) {
+            if (dataObject.msg_main.status == true) {
+                swal("Berhasil", "Insert data berhasil", "success");
+                $('#modal_form_update').modal('toggle');
+
+                // $(':input').val('');
+            }
+            else {
+                swal("Kesalahan!", "Silahkan cek kembali form!", "error");
+                //FORM VALIDATION
+                $(".text-danger").html("");
+                var data = dataObject.msg_detail.item;
+                $.each(data, function (key, value) {
+                    if (value !== null) {
+                        console.log("not null" + key);
+                        console.log(('"' + '.text-danger.' + key + '"') + value);
+
+                        $('.' + 'text-danger.' + key).html(value);
+                    }
+                })
+            }
+
+        },
+        complete: function () {
+
+            refreshTableX(TableX);
+            // swal("Sukses", "Data berhasil di Update", "success");
+
+        }
+    });
+}
+
+function insert_save() {
+    var id_jenis = $("select[name='id_jenis']").val();
+    var ket_sub_jenis = $("textarea[name='ket_sub_jenis']").val();
+    var json = { id_jenis, ket_sub_jenis };
+    //Ajax Load data from ajax
+    console.log(json);
+    console.log(TableX);
+    $.ajax({
+        url: window.url[TableX],
+        type: "POST",
+        dataType: "JSON",
+        data: json,
+        beforeSend: function () {
+            console.log("before send");
+        },
+        success: function (dataObject) {
+            if (dataObject.msg_main.status == true) {
+                swal("Berhasil", "Insert data berhasil", "success");
+                $('#modal_form_insert').modal('toggle');
+
+                // $(':input').val('');
+            }
+            else {
+                swal("Kesalahan!", "Silahkan cek kembali form!", "error");
+                //FORM VALIDATION
+                $(".text-danger").html("");
+                var data = dataObject.msg_detail.item;
+                $.each(data, function (key, value) {
+                    if (value !== null) {
+                        console.log("not null" + key);
+                        console.log(('"' + '.text-danger.' + key + '"') + value);
+
+                        $('.' + 'text-danger.' + key).html(value);
+                    }
+                })
+
+            }
+
+        },
+        complete: function () {
+            console.log("loading");
+            refreshTableX(TableX, 1);
+
+            // swal("Sukses", "Data berhasil di Update", "success");
+
+        }
+    });
+}
+
+function get_placehorder(id) {
+    var json = { id };
+    var data;
+    console.log("get_placehorder" + id)
+    $.ajax({
+        url: window.url["data_byId_sub"],
+        type: "POST",
+        dataType: "JSON",
+        data: json,
+        beforeSend: function () {
+            console.log("before send get_placehorder");
+        },
+        success: function (dataObject) {
+            if (dataObject.msg_main.status == true) {
+                data = dataObject.msg_detail.item;
+                console.log("sukses get Placehorder" + data[0].ket_jenis);
+                // $( "#ket_jenis_select :option[value='2']" ).remove();
+                $("select[name='id_jenis']").append("<option value=" + data[0].id_jenis + " selected >" + data[0].ket_jenis + "</option>");
+                $("textarea[name='ket_sub_jenis']").val(data[0].ket_sub_jenis);
+                console.log(data[0].id_sub);
+
+                // console.log("placehorder");
+            }
+            else {
+                alert("get data gagal \n" + dataObject.msg_detail.item);
+            }
+
+        },
+        complete: function () {
+            console.log("loading");
+            // $("select[name='id_jenis']").hide().html(data).fadeIn('fast');
+
+        }
+    });
+
+}
+
+function conf_delete(id) {
+    swal("apakah anda yakin ingin menghapus data?", {
+        buttons: {
+            cancel: "TIDAK",
+            catch: {
+                text: "Hapus",
+                value: "delete",
+            },
+        },
+    })
+        .then((value) => {
+            switch (value) {
+
+                case "delete":
+                    delete_byId(id);
+                    break;
+                default:
+                // swal("cancel");
+            }
+        });
+}
+
+function delete_byId(id) {
+    var json = { id };
+    $.ajax({
+        url: window.url[TableX],
+        type: "DELETE",
+        dataType: "JSON",
+        headers: { "X-HTTP-Method-Override": "DELETE" }, // X-HTTP-Method-Override set to PUT
+        data: json,
+        beforeSend: function () {
+            console.log("before send");
+        },
+        success: function (dataObject) {
+            if (dataObject.msg_main.status == true) {
+                // swal("Sukses", "Data berhasil di Hapus", "success");
+            }
+            else {
+                swal("Error", "Data tidak berhasil di Hapus", "error");
+            }
+
+        },
+        complete: function () {
+            console.log("loading");
+            refreshTableX(TableX, 1);
+
+
+        }
+    });
+}
+
+
+
