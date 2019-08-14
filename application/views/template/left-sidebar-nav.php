3b9c7fa399a14af3d86aca36223c3a0c02c85afd
rudiwawa <rudiwawa1980@gmail.com>
2019-08-14 14:09:29 +0700

    delete commend welcome.php left sidebar.php dan topbar header.php

--- application/views/template/left-sidebar-nav.php
+++ application/views/template/left-sidebar-nav.php
@@ -6,16 +6,7 @@
     <div class="scroll-sidebar">
         <!-- User profile -->
         <div class="user-profile">
-            <!-- User profile image -->
-            
-            <!-- User profile text-->
             <div class="profile-text" >
-                <!-- <h5>Markarn Doe</h5> -->
-                <!-- <a href="#" class="dropdown-toggle u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true"
-                    aria-expanded="true"><i class="mdi mdi-settings"></i></a>
-                <a href="app-email.html" class="" data-toggle="tooltip" title="Email"><i class="mdi mdi-gmail"></i></a>
-                <a href="pages-login.html" class="" data-toggle="tooltip" title="Logout"><i
-                        class="mdi mdi-power"></i></a> -->
                 <div class="dropdown-menu animated flipInY">
                     <!-- text-->
                     <a href="#" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
